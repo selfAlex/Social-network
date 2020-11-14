cd4525fafff4f2909d314d2c8bdfd5b721dc207b
Alex <alex20k.x@gmail.com>
2020-11-14 20:58:34 +0600

    Initial commit

---
+++ core/core/settings.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'y+$w$u45@%c2de$^4v1gb^&8h9v!_ikgdsg59cbqt6nh($*2bl'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+
+]
+
+MIDDLEWARE = [
+
+]
+
+ROOT_URLCONF = 'core.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
+        'DIRS': [],
+        'APP_DIRS': True,
+
+        'OPTIONS': {
+            'context_processors': [
+
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC+6'
