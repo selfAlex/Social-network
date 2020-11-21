34f1b1b0d2c47432a8a4d4cea4403c5c4512a061
Alex <alex20k.x@gmail.com>
2020-11-21 19:52:28 +0600

    Overridden user model

--- core/app/models.py
+++ core/app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
+
+class OverrodeUserManager(BaseUserManager):
+    def create_user(self, email, password):
+        email = self.normalize_email(email)
+
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    objects = OverrodeUserManager()
