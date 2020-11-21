f60147c653daf8f17ae30ed0e8350e87fdbd7198
Alex <alex20k.x@gmail.com>
2020-11-21 19:50:40 +0600

    Moved to the views folder

--- core/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def index_page(request):
-    return render(request, 'app/index.html')
-
-
-def profile_page(request):
-    return render(request, 'app/profile.html')
