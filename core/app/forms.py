5bbba17144bc7aa309a250dd92f33c4ec46ddbe5
Alex <alex20k.x@gmail.com>
2020-12-05 10:11:50 +0600

    Unique ID

--- core/app/forms.py
+++ core/app/forms.py
@@ -2,11 +2,12 @@ from django import forms
 
 
 class SignInForm(forms.Form):
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'uk-input'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'uk-input'}))
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'uk-input', 'id': 'signin_email'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'uk-input',
+                                                                                   'id': 'signin_password'}))
 
 
 class SignUpForm(forms.Form):
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'uk-input'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'uk-input'}))
-
+    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'uk-input', 'id': 'signup_email'}))
+    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'uk-input',
+                                                                                   'id': 'signup_password'}))
