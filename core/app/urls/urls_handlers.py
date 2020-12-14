52e53f61e567f86991d8c4654703236d463c1093
Alex <alex20k.x@gmail.com>
2020-12-14 19:34:03 +0600

    url_handler for verify

--- core/app/urls/urls_handlers.py
+++ core/app/urls/urls_handlers.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from ..views import views_handlers
+from ..views.views_handlers import views_handlers
 
 urlpatterns = [
 
     path('signin', views_handlers.signin_handler, name='signin_handler'),
     path('signup', views_handlers.signup_handler, name='signup_handler'),
+
+    path('verify/<str:email>/<str:password>', views_handlers.verify_user, name='verify_user'),
 
     path('logout', views_handlers.logout_handler, name='logout_handler')
 
