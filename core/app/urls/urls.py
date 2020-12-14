23c6f70a7988e07385b33881b8f63d49a90efed6
Alex <alex20k.x@gmail.com>
2020-12-14 19:33:35 +0600

    pep8 line

--- core/app/urls/urls.py
+++ core/app/urls/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 
     path('', views.index_page, name='index_page'),
 
-    path('profile', views.profile_page, name='profile_page')
+    path('profile', views.profile_page, name='profile_page'),
 
 ]
