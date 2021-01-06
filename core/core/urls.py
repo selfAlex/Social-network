d04fd23fbd40d338e82a7a8e8671497518231ae5
Alex <alex20k.x@gmail.com>
2021-01-06 12:50:21 +0600

    Django-Allauth urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
 
+from allauth.socialaccount import providers
+from importlib import import_module
+
+providers_urlpatterns = []
+
+for provider in providers.registry.get_list():
+    prov_mod = import_module(provider.get_package() + '.urls')
+    providers_urlpatterns += getattr(prov_mod, 'urlpatterns', [])
+
 urlpatterns = [
 
     path('', include('app.urls.urls')),
 
-    path('handle/', include('app.urls.urls_handlers'))
+    path('handle/', include('app.urls.urls_handlers')),
+
+    path('socialauth/', include(providers_urlpatterns)),
 
 ]
