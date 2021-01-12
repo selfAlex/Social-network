d0f391c7e14a497db699709dd2c6b2dc37c293c1
Alex <alex20k.x@gmail.com>
2021-01-12 19:40:11 +0600

    API page

--- core/app/urls/urls.py
+++ core/app/urls/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path('', views.index_page, name='index_page'),
 
     path('profile', views.profile_page, name='profile_page'),
+    path('profile_edit', views.profile_edit_page, name='profile_edit_page'),
+
+    path('messenger', views.messenger_page, name='messenger_page'),
+
+    path('api', views.api_page, name='api_page')
 
 ]
