350243248268ded849cdcce845d6f5be50f80b05
Alex <alex20k.x@gmail.com>
2021-01-18 19:41:34 +0600

    Fields: images, custom password, date_joined

--- core/app/models.py
+++ core/app/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.base_user import BaseUserManager
 
+from django.utils.functional import lazy
+
+from datetime import datetime
+
+from django.utils.translation import gettext_lazy as _
+
 
 class OverrodeUserManager(BaseUserManager):
     def create_user(self, email, password):
         email = self.normalize_email(email)
 
-        user = self.model(email=email)
+        date_joined = datetime.now()
+
+        user = self.model(email=email, date_joined=date_joined)
         user.set_password(password)
         user.save()
 
@@ -15,7 +23,16 @@ class OverrodeUserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    first_name = models.TextField()
+    surname = models.TextField()
+
     email = models.EmailField(unique=True)
+
+    password = models.CharField(_('password'), max_length=128, null=True)
+
+    images = models.TextField()
+
+    date_joined = models.DateTimeField(null=True)
 
     USERNAME_FIELD = 'email'
     objects = OverrodeUserManager()
