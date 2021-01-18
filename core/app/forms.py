b368ee27a903faf72486af06382361898aa61377
Alex <alex20k.x@gmail.com>
2021-01-18 19:36:42 +0600

    Forms for editing a profile

--- core/app/forms.py
+++ core/app/forms.py
@@ -11,3 +11,11 @@ class SignUpForm(forms.Form):
     email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'uk-input', 'id': 'signup_email'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'uk-input',
                                                                                    'id': 'signup_password'}))
+
+
+class ProfileEditForm(forms.Form):
+    first_name = forms.CharField(label='First name', widget=forms.TextInput(attrs={'class': 'uk-input',
+                                                                                   'required': False}))
+
+    surname = forms.CharField(label='Surname', widget=forms.TextInput(attrs={'class': 'uk-input',
+                                                                             'required': False}))
